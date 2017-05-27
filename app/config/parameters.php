6496dc27d2b2d1bea0fd9811fb6aa73e9395c7f9
kseniia <ksenyagrabevnyk@gmail.com>
2017-05-27 18:44:21 +0300

    para

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,19 +1,13 @@
 <?php
 
-$db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
-$container->setParameter('database_driver', 'pdo_mysql'); 
-$container->setParameter('database_host', $db['host']); 
-$container->setParameter('database_port', '~');
-$container->setParameter('database_name', trim($db['path'], '/')); 
-$container->setParameter('database_user', $db['user']); 
-$container->setParameter('database_password', $db['pass']);
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// Using Mandrill to send email
-$container->setParameter('mailer_transport', 'smtp');
-$container->setParameter('mailer_host', 'smtp.mandrillapp.com');
-$container->setParameter('mailer_user', $_ENV['MANDRILL_USERNAME']);
-$container->setParameter('mailer_password', $_ENV['MANDRILL_APIKEY']);
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"],1);
 
-// Other settings
-$container->setParameter('locale', 'en');
-$container->setParameter('secret', 'your_secret');
+mysqli_connect($server, $username, $password);
+
+
+//mysqli_select_db($db);
